e827a4172fab04c4a62a2f81b650bc9a6f683044
YoanGuillot <129792552+YoanGuillot@users.noreply.github.com>
2025-02-20 19:09:56 +0100

    20/02/2025

--- includes/dashboard.inc.php
+++ includes/dashboard.inc.php
@@ -87,24 +87,7 @@ if (isset($_GET['idtournoi'])){
 				</div>
 				
 			</div>
-		
-			<div class="bar-bottom">
-				<ul class="uk-subnav uk-flex uk-flex-center uk-child-width-1-5" data-uk-grid>
-					<li>
-						<a href="#" class="uk-icon-link" data-uk-icon="icon: home" title="Home" data-uk-tooltip></a>
-					</li>
-					<li>
-						<a href="#" class="uk-icon-link" data-uk-icon="icon: settings" title="Settings" data-uk-tooltip></a>
-					</li>
-					<li>
-						<a href="#" class="uk-icon-link" data-uk-icon="icon: social"  title="Social" data-uk-tooltip></a>
-					</li>
-					
-					<li>
-						<a href="#" class="uk-icon-link" data-uk-tooltip="Sign out" data-uk-icon="icon: sign-out"></a>
-					</li>
-				</ul>
-			</div>
+	
 <?php 
 	//Fin condition accueil
 	}
